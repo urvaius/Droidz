4ab174afb2baff99795a914f2176ec35a87c81a0
Dave Arneson <urvaius@gmail.com>
2012-08-15 14:09:20 -0700

    first commit on here

---
+++ Droidz/src/com/arne5/droidz/MainGamePanel.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.arne5.droidz;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * @author impaler
+ * This is the main surface that handles the ontouch events and draws
+ * the image to the screen.
+ */
+public class MainGamePanel extends SurfaceView implements
+		SurfaceHolder.Callback {
+
+	private static final String TAG = MainGamePanel.class.getSimpleName();
+	
+	private MainThread thread;
+
+	public MainGamePanel(Context context) {
+		super(context);
+		// adding the callback (this) to the surface holder to intercept events
+		getHolder().addCallback(this);
+		
+		// create the game loop thread
+		thread = new MainThread(getHolder(), this);
+		
+		// make the GamePanel focusable so it can handle events
+		setFocusable(true);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// at this point the surface is created and
+		// we can safely start the game loop
+		thread.setRunning(true);
+		thread.start();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d(TAG, "Surface is being destroyed");
+		// tell the thread to shut down and wait for it to finish
+		// this is a clean shutdown
+		boolean retry = true;
+		while (retry) {
+			try {
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				// try again shutting down the thread
+			}
+		}
+		Log.d(TAG, "Thread was shut down cleanly");
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			if (event.getY() > getHeight() - 50) {
+				thread.setRunning(false);
+				((Activity)getContext()).finish();
+			} else {
+				Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
+			}
+		}
+		return super.onTouchEvent(event);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+	}
+
+}
---
+++ Droidz/src/com/arne5/droidz/MainThread.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.arne5.droidz;
+
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+
+/**
+ * @author impaler
+ *
+ * The Main thread which contains the game loop. The thread must have access to 
+ * the surface view and holder to trigger events every game tick.
+ */
+public class MainThread extends Thread {
+	
+	private static final String TAG = MainThread.class.getSimpleName();
+
+	// Surface holder that can access the physical surface
+	private SurfaceHolder surfaceHolder;
+	// The actual view that handles inputs
+	// and draws to the surface
+	private MainGamePanel gamePanel;
+
+	// flag to hold game state 
+	private boolean running;
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public MainThread(SurfaceHolder surfaceHolder, MainGamePanel gamePanel) {
+		super();
+		this.surfaceHolder = surfaceHolder;
+		this.gamePanel = gamePanel;
+	}
+
+	@Override
+	public void run() {
+		long tickCount = 0L;
+		Log.d(TAG, "Starting game loop");
+		while (running) {
+			tickCount++;
+			// update game state 
+			// render state to the screen
+		}
+		Log.d(TAG, "Game loop executed " + tickCount + " times");
+	}
+	
+}
